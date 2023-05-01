eeafd03e028515b6206a75aa4e0dc7699b789f70
Wesley Reis <wesleydno2011@gmail.com>
2023-05-01 19:45:24 -0300

    feat: configured postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_CONNECTION,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
